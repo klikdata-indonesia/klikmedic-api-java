71316c4614e5656c57cc932dfa271ff14dd74f93
hermandev <hermantolakoro@gmail.com>
2023-09-08 20:38:57 +0800

    create

---
+++ src/main/java/io/github/klikdata/Klikmedic.java
@@ -0,0 +1,17 @@
+package io.github.klikdata;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class Klikmedic {
+    private static final Logger logger = LoggerFactory.getLogger(Klikmedic.class);
+
+    public static void main( String[] args )
+    {
+        logger.info("Hello World!");
+    }
+}
---
+++ src/test/java/io/github/klikdata/KlikmedicTest.java
@@ -0,0 +1,7 @@
+package io.github.klikdata;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KlikmedicTest {
+
+}
