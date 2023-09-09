8052913e6bdc1b9cb8b83c20c0764f593ca72542
hermandev <hermantolakoro@gmail.com>
2023-09-09 21:27:33 +0800

    create testing

--- src/test/java/io/github/klikdata/KlikmedicTest.java
+++ src/test/java/io/github/klikdata/KlikmedicTest.java
@@ -1,7 +1,20 @@
 package io.github.klikdata;
 
+import io.github.klikdata.impl.KlikmedicClient;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KlikmedicTest {
+    private static final Logger log = LoggerFactory.getLogger(KlikmedicTest.class);
+
+    @Test
+    public void kmSayHelloTests() {
+        KlikmedicClient km = new KlikmedicClient();
+        log.debug(km.sayHello());
+        assertEquals("Hello BPJS", km.sayHello());
+    }
 
 }
