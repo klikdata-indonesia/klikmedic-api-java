dcf245436a9734ddc23e2441ff3c9165cd16cbf2
hermandev <hermantolakoro@gmail.com>
2023-09-09 21:26:59 +0800

    add Klikmedic interface

--- src/main/java/io/github/klikdata/Klikmedic.java
+++ src/main/java/io/github/klikdata/Klikmedic.java
@@ -7,11 +7,6 @@ import org.slf4j.LoggerFactory;
  * Hello world!
  *
  */
-public class Klikmedic {
-    private static final Logger logger = LoggerFactory.getLogger(Klikmedic.class);
-
-    public static void main( String[] args )
-    {
-        logger.info("Hello World!");
-    }
+public interface Klikmedic {
+    public String sayHello();
 }
